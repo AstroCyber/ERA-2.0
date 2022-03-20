53dd76a2afdd671e58d837663e6c8fb081820b9f
AstroCyber <AstroGame1928@gmail.com>
2022-03-20 13:25:00 +0530

    Create stt.py

---
+++ stt.py
@@ -0,0 +1,34 @@
+import speech_recognition as sr
+import imp
+
+#getting variable recognizer
+recognizer = imp.recognizer
+#getting variable speaker
+speaker = imp.speaker
+
+def get():
+    try:
+        #listening your voice and saving it in source2 variable
+        with sr.Microphone() as source2:
+            print("Listening..")
+            speaker.say("Listening")
+            speaker.runAndWait()
+            #adjusting for noices
+            recognizer.adjust_for_ambient_noise(source2, duration=0.2)
+            #saving the sound in mp3 format
+            audio2 = recognizer.listen(source2)
+            #recognizing the mp3 voice to text
+            MyText = recognizer.recognize_google(audio2)
+            #converting MyText variable to lower
+            MyText = MyText.lower()
+            print(MyText)
+        return MyText
+
+    except sr.RequestError as e:
+        print("Request-Error")
+
+    except sr.UnknownValueError as ue:
+        print("Unknown-Value-Error")
+
+    except OSError:
+        print("OS-Error")
